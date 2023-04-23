a9f543a169764ef1a91a650daca609aad844fcbd
Jackson <jdmeils@uwm.edu>
2023-04-23 17:40:13 -0500

    implement AccountSettings#get

--- TA_Scheduling_App/views/account_settings.py
+++ TA_Scheduling_App/views/account_settings.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from TA_Scheduling_App.models import User
 
 
 class AccountSettings(View):
     def get(self, request):
-        return render(request, "profile.html", {})
+        # check user is logged in
+        if not request.session.get("is_authenticated"):
+            return redirect("login")
+
+        # get known profile information for user
+        active_user = User.objects.get(USER_ID=request.session["user_id"])
+        initial_date = active_user.BIRTH_DATE.isoformat()
+        return render(request, "profile.html", {'initial': active_user, 'initial_date': initial_date})
