429f33b6a9e24904ba36705b5b274d1567e990b2
Jackson <jdmeils@uwm.edu>
2023-04-23 20:52:25 -0500

    implement user-management#post, functional page

--- TA_Scheduling_App/views/user_management.py
+++ TA_Scheduling_App/views/user_management.py
@@ -1,10 +1,18 @@
-from django.core.exceptions import PermissionDenied
+from django.core.exceptions import PermissionDenied, BadRequest
 from django.shortcuts import render, redirect
 from django.views import View
 from TA_Scheduling_App.models import User
 
 
 class UserManagement(View):
+    def get_users(self, request):
+        users = User.objects.all()
+        # annotate logged in user with flag so we do not render 'delete' button
+        user_id = request.session.get("user_id")
+        for user in users:
+            user.deletable = user.USER_ID != user_id
+        return users
+
     def get(self, request):
         # check user is logged in
         if not request.session.get("is_authenticated"):
@@ -14,13 +22,24 @@ class UserManagement(View):
             raise PermissionDenied("You are not permitted to manage other users")
 
         # get all users from database for display
-        users = User.objects.all()
-        # users = [ users.first()]
-
-        # annotate logged in user with flag so we do not render 'delete' button
-        user_id = request.session.get("user_id")
-        for user in users:
-            user.deletable = user.USER_ID != user_id
-
+        users = self.get_users(request)
         return render(request, "user-management.html", {'users': users})
+
+    def post(self, request):
+        # a user deletion has been requested
+        # validate access to this operation
+        if request.session.get("user_role") != "ADMIN":
+            raise PermissionDenied("You are not permitted to delete users")
+
+        try:
+            # get relevant user from database
+            target = User.objects.get(USER_ID=request.POST["deleteUserId"])
+            deleted = f"User {target.USER_ID}: {target.EMAIL} has been deleted."
+            target.delete()
+
+            users = self.get_users(request)
+            return render(request, "user-management.html", {'users': users, 'status': deleted})
+        except Exception as e:
+            print(f"Bad Request to POST user-management: {repr(e)}")
+            raise BadRequest()
 
