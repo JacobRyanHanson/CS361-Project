1f5a9c86bf7ea7c9c84fc1de578b17097863e1c2
Jackson <jdmeils@uwm.edu>
2023-04-23 20:19:51 -0500

    implement UserManagement#get

--- TA_Scheduling_App/views/user_management.py
+++ TA_Scheduling_App/views/user_management.py
@@ -1,23 +1,26 @@
-from django.shortcuts import render
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import render, redirect
 from django.views import View
-from dataclasses import dataclass
-
-
-@dataclass
-class User:
-    id: int
-    type: str
-    firstName: str
-    lastName: str
-    email: str
-    phoneNumber: str
-    address: str
-    dateOfBirth: str
+from TA_Scheduling_App.models import User
 
 
 class UserManagement(View):
     def get(self, request):
-        sample = User(1, "ADMIN", "Example", "User", "example@example.com", "000-000-0000", "null street", "00-00-0000")
-        users = [sample, sample, sample]
+        # check user is logged in
+        if not request.session.get("is_authenticated"):
+            return redirect("login")
+        # check user is admin - restricted access page
+        if request.session.get("user_role") != "ADMIN":
+            raise PermissionDenied("You are not permitted to manage other users")
+
+        # get all users from database for display
+        users = User.objects.all()
+        # users = [ users.first()]
+
+        # annotate logged in user with flag so we do not render 'delete' button
+        user_id = request.session.get("user_id")
+        for user in users:
+            user.deletable = user.USER_ID != user_id
+
         return render(request, "user-management.html", {'users': users})
 
