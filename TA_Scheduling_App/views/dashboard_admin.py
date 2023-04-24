7e36179088c3ec8a5caef8d78953a08068ef670a
Jackson <jdmeils@uwm.edu>
2023-04-23 21:14:43 -0500

    add basic auth to dashboards

--- TA_Scheduling_App/views/dashboard_admin.py
+++ TA_Scheduling_App/views/dashboard_admin.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 
 
 class DashboardAdmin(View):
     def get(self, request):
+        # verify user is logged in as admin
+        if not request.session.get("is_authenticated"):
+            return redirect("login")
+        if request.session.get("user_role") != "ADMIN":
+            # send to home per state machine
+            return redirect("home")
         return render(request, "dashboard-admin.html", {})
--- TA_Scheduling_App/views/dashboard_ta.py
+++ TA_Scheduling_App/views/dashboard_ta.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 
 
 class DashboardTA(View):
     def get(self, request):
+        # verify user is logged in as admin
+        if not request.session.get("is_authenticated"):
+            return redirect("login")
+        if request.session.get("user_role") != "TA":
+            # send to home per state machine
+            return redirect("home")
         return render(request, "dashboard-ta.html", {})
