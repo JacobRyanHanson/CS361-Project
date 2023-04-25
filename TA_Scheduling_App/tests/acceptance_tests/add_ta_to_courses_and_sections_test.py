ca5939ea307114afe03a7c4a4c11cf70dd68e093
JacobRyanHanson <jacobryanhanson@gmail.com>
2023-04-25 17:37:22 -0500

    Add acceptance tests for duplicate assignments

--- TA_Scheduling_App/tests/acceptance_tests/add_ta_to_courses_and_sections_test.py
+++ TA_Scheduling_App/tests/acceptance_tests/add_ta_to_courses_and_sections_test.py
@@ -386,6 +386,108 @@ class InvalidSectionAssignment(TestCase):
         with self.assertRaises(UnboundLocalError, msg="Section does not exist."):
             response = self.client.post("/ta-assignments/", section_assignment_form_data, follow=True)
 
+class InvalidDuplicateAssignment(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+        self.admin = User(
+            ROLE='ADMIN',
+            FIRST_NAME='John',
+            LAST_NAME='Doe',
+            EMAIL='admin@example.com',
+            PASSWORD_HASH='ad_password',
+            PHONE_NUMBER='555-123-4567',
+            ADDRESS='123 Main St',
+            BIRTH_DATE=date(1990, 1, 1)
+        )
+
+        self.admin.save()
+
+        self.instructor = User(
+            ROLE='INSTRUCTOR',
+            FIRST_NAME='Will',
+            LAST_NAME='Doe',
+            EMAIL='instructor@example.com',
+            PASSWORD_HASH='ad_password',
+            PHONE_NUMBER='555-123-4567',
+            ADDRESS='123 Main St',
+            BIRTH_DATE=date(1990, 1, 1)
+        )
+
+        self.instructor.save()
+
+        self.ta = User(
+            ROLE='TA',
+            FIRST_NAME='Jane',
+            LAST_NAME='Doe',
+            EMAIL='ta@example.com',
+            PHONE_NUMBER='555-987-6543',
+            ADDRESS='789 Pine St',
+            BIRTH_DATE=date(1998, 6, 15)
+        )
+
+        self.ta.save()
+
+        self.course = Course(
+            COURSE_NUMBER=151,
+            INSTRUCTOR=self.instructor,
+            COURSE_NAME='Introduction to Computer Science',
+            COURSE_DESCRIPTION='An introductory course to the world of computer science.',
+            SEMESTER='Fall 2023',
+            PREREQUISITES='',
+            DEPARTMENT='Computer Science'
+        )
+
+        self.course.save()
+
+        self.section = Section(
+            SECTION_NUMBER=1,
+            COURSE=self.course,
+            BUILDING='Tech Building',
+            ROOM_NUMBER='111',
+            SECTION_START=time(9, 30),
+            SECTION_END=time(10, 20)
+        )
+
+        self.section.save()
+
+        self.course_assignment_form_data = {
+            'course_id': 1,
+            'course_ta_email': 'ta@example.com',
+            'course_ta_select': 'true'
+        }
+
+        self.other_course_assignment_form_data = {
+            'course_id': 2,
+            'course_ta_email': 'ta@example.com',
+            'course_ta_select': 'true'
+        }
+
+        self.section_assignment_form_data = {
+            'course_id': 1,
+            'section_ta_email': 'ta@example.com',
+            'section_id': 1
+        }
+
+        self.credentials = {
+            "email": "admin@example.com",
+            "password": "ad_password"
+        }
+
+        self.client.post("/", self.credentials, follow=True)
+
+    def test_duplicate_ta_assignment_to_course(self):
+        response = self.client.post("/ta-assignments/", self.course_assignment_form_data, follow=True)
+        response = self.client.post("/ta-assignments/", self.course_assignment_form_data, follow=True)
+        self.assertEqual(str(response.context['status']), "Duplicate assignment of TA to course failed")
+
+    def test_duplicate_ta_assignment_to_section(self):
+        # Assign TA to course
+        response = self.client.post("/ta-assignments/", self.course_assignment_form_data, follow=True)
+        # Duplicate section assignments
+        response = self.client.post("/ta-assignments/", self.section_assignment_form_data, follow=True)
+        response = self.client.post("/ta-assignments/", self.section_assignment_form_data, follow=True)
+        self.assertEqual(str(response.context['status']), "Duplicate assignment of TA to section failed")
 
 
 
