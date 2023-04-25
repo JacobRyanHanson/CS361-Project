bf4890745dce6d9a9bf6558c14b9357c30b6b543
JacobRyanHanson <jacobryanhanson@gmail.com>
2023-04-25 09:55:00 -0500

    Add all acceptance tets for view all courses and sections

--- TA_Scheduling_App/tests/acceptance_tests/view_all_courses_and_sections.py
+++ TA_Scheduling_App/tests/acceptance_tests/view_all_courses_and_sections.py
@@ -4,6 +4,10 @@ from bs4 import BeautifulSoup
 from django.test import TestCase, Client
 from TA_Scheduling_App.models import Course, Section, User
 
+
+def contains_text(tag, text):
+    return tag.string is not None and text.strip() in tag.string.strip()
+
 class ViewAllSuccess(TestCase):
     def setUp(self):
         self.client = Client()
@@ -121,18 +125,15 @@ class ViewAllSuccess(TestCase):
             }
         ]
 
-    def contains_text(self, tag, text):
-        return tag.string is not None and text.strip() in tag.string.strip()
-
     def test_all_courses_are_visible(self):
         for course in self.expected_courses:
             for key, value in course.items():
-                self.assertIsNotNone(self.soup.find(lambda tag: self.contains_text(tag, value)), f"{key} {value} not found")
+                self.assertIsNotNone(self.soup.find(lambda tag: contains_text(tag, value)), f"{key} {value} not found")
 
     def test_all_sections_are_visible(self):
         for section in self.expected_sections:
             for key, value in section.items():
-                self.assertIsNotNone(self.soup.find(lambda tag: self.contains_text(tag, value)), f"{key} {value} not found")
+                self.assertIsNotNone(self.soup.find(lambda tag: contains_text(tag, value)), f"{key} {value} not found")
 
     def test_all_instructors_are_visible(self):
         expected_instructors = [
@@ -144,7 +145,7 @@ class ViewAllSuccess(TestCase):
 
         for instructor in expected_instructors:
             for key, value in instructor.items():
-                self.assertIsNotNone(self.soup.find(lambda tag: self.contains_text(tag, value)), f"{key} {value} not found")
+                self.assertIsNotNone(self.soup.find(lambda tag: contains_text(tag, value)), f"{key} {value} not found")
 
         # Find value of input field (not directly in HTML)
         self.assertIsNotNone(self.soup.find('input', {'type': 'email', 'value': 'instructor@example.com'}), f"email not found")
@@ -174,6 +175,80 @@ class ViewAllSuccess(TestCase):
                 'SECTION_END': section.SECTION_END.strftime('%H:%M').lstrip('0')
             }
             self.assertIn(section_data, self.expected_sections, f"Unexpected section found: {section_data}")
+
+class ViewAllFail(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+        self.admin = User(
+            ROLE='ADMIN',
+            FIRST_NAME='John',
+            LAST_NAME='Doe',
+            EMAIL='admin@example.com',
+            PASSWORD_HASH='ad_password',
+            PHONE_NUMBER='555-123-4567',
+            ADDRESS='123 Main St',
+            BIRTH_DATE=date(1990, 1, 1)
+        )
+
+        self.admin.save()
+
+        self.credentials = {
+            "email": "admin@example.com",
+            "password": "ad_password"
+        }
+
+        self.course = Course.objects.create(
+            COURSE_NUMBER=999,
+            INSTRUCTOR=None,
+            COURSE_NAME='Test Course',
+            COURSE_DESCRIPTION='A test course.',
+            SEMESTER='Fall 2023',
+            PREREQUISITES='',
+            DEPARTMENT='Computer Science'
+        )
+
+        self.course.save()
+
+        self.section = Section.objects.create(
+            SECTION_NUMBER=894,
+            COURSE=self.course,
+            BUILDING='Tech Building',
+            ROOM_NUMBER='999',
+            SECTION_START=time(9, 30),
+            SECTION_END=time(10, 20)
+        )
+
+        self.section.save()
+
+        self.client.post("/", self.credentials, follow=True)
+
+        self.response = self.client.get("/ta-assignments/", follow=True)
+        self.soup = BeautifulSoup(self.response.content, 'html.parser')
+
+    def test_no_courses_in_system_and_none_visible(self):
+        courses = Course.objects.all()
+
+        if len(courses) == 0:
+            self.assertIsNone(self.soup.find(lambda tag: tag.name == "h5" and contains_text(tag, "Course")), "A course was found")
+
+    def test_no_sections_in_system_and_none_visible(self):
+        sections = Section.objects.all()
+
+        if len(sections) == 0:
+            self.assertIsNone(self.soup.find(lambda tag: tag.name == "h5" and contains_text(tag, "Section")), "A section was found")
+
+    def test_course_not_visible_when_deleted(self):
+        self.course.delete()
+        self.response = self.client.get("/ta-assignments/", follow=True)
+        self.soup = BeautifulSoup(self.response.content, 'html.parser')
+        self.assertIsNone(self.soup.find(lambda tag: contains_text(tag, 'Test Course')), "Deleted course was found")
+
+    def test_section_not_visible_when_deleted(self):
+        self.section.delete()
+        self.response = self.client.get("/ta-assignments/", follow=True)
+        self.soup = BeautifulSoup(self.response.content, 'html.parser')
+        self.assertIsNone(self.soup.find(lambda tag: contains_text(tag, '894')), "Deleted section was found")
 
 
 
