70f31ae33fbe56c7f8d7c0728527ac19330d7e27
Jackson <jdmeils@uwm.edu>
2023-04-25 20:49:18 -0500

    refactor create_user_test to follow PBIs, pull out repeated setup code

--- TA_Scheduling_App/tests/acceptance_tests/create_user_test.py
+++ TA_Scheduling_App/tests/acceptance_tests/create_user_test.py
@@ -1,42 +1,29 @@
-from datetime import date, datetime
-import os
-from unittest.mock import MagicMock
-
-import django
+from datetime import date
 from django.test import TestCase, Client
-
-from TA_Scheduling_App.models import User, Course
-
-# Set up the Django settings module for testing
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TA_Scheduling_Project.settings')
-django.setup()
+from TA_Scheduling_App.models import User
 
 
-class TestUserCreation(TestCase):
-    monkey = None
+def login_as_admin(client: Client):
+    admin = User(ROLE='ADMIN',
+                 FIRST_NAME='John',
+                 LAST_NAME='Doe',
+                 EMAIL='admin@example.com',
+                 PASSWORD_HASH='ad_password',
+                 PHONE_NUMBER='555-123-4567',
+                 ADDRESS='123 Main St',
+                 BIRTH_DATE=date(1990, 1, 1))
+    admin.save()
+    client.post("/", {
+        "email": admin.EMAIL,
+        "password": admin.PASSWORD_HASH
+    })
+
+
+class CreationSuccess(TestCase):
 
     def setUp(self):
         self.monkey = Client()
-
-        self.admin = User(
-            ROLE='ADMIN',
-            FIRST_NAME='John',
-            LAST_NAME='Doe',
-            EMAIL='admin@example.com',
-            PASSWORD_HASH='ad_password',
-            PHONE_NUMBER='555-123-4567',
-            ADDRESS='123 Main St',
-            BIRTH_DATE=date(1990, 1, 1)
-        )
-
-        self.admin.save()
-
-        self.credentials = {
-            "email": "admin@example.com",
-            "password": "ad_password"
-        }
-
-        self.monkey.post("/", self.credentials, follow=True)
+        login_as_admin(self.monkey)
 
     def test_AddNewTA(self):
         response = self.monkey.post('/user-creation/', {'role': "TA",
@@ -57,6 +44,12 @@ class TestUserCreation(TestCase):
                                                         'address': "102 Drive",
                                                         'birthDate': date(1990, 1, 2)})
         self.assertEqual(response.context['status'], 'Successfully created the user.')
+
+
+class CreationFail(TestCase):
+    def setUp(self):
+        self.monkey = Client()
+        login_as_admin(self.monkey)
 
     def test_InvalidEmail(self):
         response = self.monkey.post('/user-creation/', {'role': "TA",
