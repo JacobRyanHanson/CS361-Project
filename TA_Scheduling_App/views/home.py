45066ba5516ae8e99297a6cb1ef2db316c5bd290
MaramS03 <mssaleh@uwm.edu>
2023-05-03 17:37:35 -0500

    Merged dashboard and home

--- TA_Scheduling_App/views/home.py
+++ TA_Scheduling_App/views/home.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render, redirect
 from django.views import View
 
+
 class Home(View):
     def get(self, request):
-        if not request.session.get('is_authenticated'):
+        if not request.session.get("is_authenticated"):
             return redirect("login")
-        return render(request, "home.html", {})
+        if request.session.get("user_role") == "ADMIN":
+            return render(request, "dashboard-admin.html", {})
+        elif request.session.get("user_role") == "INSTRUCTOR":
+            return render(request, "dashboard-instructor.html", {})
+        elif request.session.get("user_role") == "TA":
+            return render(request, "dashboard-ta.html", {})
