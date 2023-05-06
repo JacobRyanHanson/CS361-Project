bf1a85d9ca3fdcdf84576f662ff0eeb7a3f22439
MaramS03 <mssaleh@uwm.edu>
2023-05-06 00:14:16 -0500

    Updated dashboards

--- TA_Scheduling_App/views/dashboard_admin.py
+++ TA_Scheduling_App/views/dashboard_admin.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
 from django.views import View
+from TA_Scheduling_App.models import User
 
 
 class DashboardAdmin(View):
     def get(self, request):
-        if not request.session.get("is_authenticated"):
+        if not request.session.get('is_authenticated'):
             return redirect("login")
-        if request.session.get("user_role") != "ADMIN":
-            return redirect("home")
-        return render(request, "dashboard-admin.html", {})
+
+        user = User.objects.get(USER_ID=request.session["user_id"])
+        return render(request, "dashboard-admin.html", {'role': user.ROLE})
