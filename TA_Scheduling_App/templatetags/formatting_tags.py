0bd82db178bc32ffca546e6869d71f2fa15dd2b4
Jackson <jdmeils@uwm.edu>
2023-05-07 22:58:28 -0500

    update ta assignments template
    - instructors: only view courses assigned to
    - TAs: only view sections assigned to
    - implement most dropdowns, may conflict with other's implementations

--- TA_Scheduling_App/templatetags/formatting_tags.py
+++ TA_Scheduling_App/templatetags/formatting_tags.py
@@ -1,9 +1,23 @@
 import datetime
+from TA_Scheduling_App.models import User, Course
 
 from django import template
 
 register = template.Library()
 
+
 @register.filter("html_date")
 def html_date(date: datetime.date):
     return date.isoformat()
+
+
+@register.filter("full_name")
+def full_name(user: User):
+    if not user:
+        return "None"
+    return f"{user.FIRST_NAME} {user.LAST_NAME}"
+
+
+@register.filter("course_name")
+def course_name(course: Course):
+    return f"{course.DEPARTMENT} {course.COURSE_NUMBER} - {course.COURSE_NAME}"
